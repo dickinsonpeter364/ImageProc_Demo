352518eb16871ad0992b1ad3a3bf1b666ef77716
dickinsonpeter364 <dickinsonpeter364@gmail.com>
2026-01-21 07:56:16 +0000

    Add project files.

---
+++ gg.bat
@@ -0,0 +1,8 @@
+echo "# ImageProc3" >> README.md
+git init
+git add .
+
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/dickinsonpeter364/ImageProc2.git
+git push -u origin master
